b297cc07cf8cdb566bd92d0c1f1fb2bdb5cbda1b
Hosam Youssef <hosamyoussef303@gmail.com>
2024-01-14 03:48:47 +0200

    edit links

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,10 +1,16 @@
-export default function Footer(){
-    return(
+import React from 'react';
+
+export default function Footer() {
+    const handleIconClick = (url) => {
+        window.open(url,'_blank');
+    };
+
+    return (
         <div className="footer--container">
-            <i className="fa fa-twitter"></i>
-            <i className="fa fa-facebook"></i>
-            <i className="fa fa-instagram"></i>
-            <i className="fa fa-github"></i>        
+            <i className="fa fa-twitter" onClick={() => handleIconClick('https://twitter.com/hosamusf')}></i>
+            <i className="fa fa-facebook" onClick={() => handleIconClick('https://facebook.com/hosammyoussef')}></i>
+            <i className="fa fa-linkedin" onClick={() => handleIconClick('https://www.linkedin.com/in/hosammohamed')}></i>
+            <i className="fa fa-github" onClick={() => handleIconClick('https://github.com/hosamusf')}></i>
         </div>
-    )
+    );
 }
